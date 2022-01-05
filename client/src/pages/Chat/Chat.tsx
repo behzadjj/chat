2b244abb09185cc375388fc7a98e84172cd9cfb1
behzad <b.jafari@tsetmc.com>
2022-01-05 09:03:27 +0330

    Commit client side changes for join and create form.

--- client/src/pages/Chat/Chat.tsx
+++ client/src/pages/Chat/Chat.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { io } from "socket.io-client";
+import axios from "axios";
 
-import { Gate, UsersList, MessageForm } from "./components";
+import { Gate, UsersList, MessageForm, GateModes } from "./components";
 import { UserMessages } from "./components/UserMessages";
 
 let socketChannel: any = undefined;
@@ -9,27 +10,71 @@ let socketChannel: any = undefined;
 export const Chat = () => {
   const [joined, setJoined] = useState(false);
 
+  const [gateMode, setGateMode] = useState<GateModes>();
+
+  const [room, setRoom] =
+    useState<{ roomName: string; roomId: string; userId: string }>();
+
   const [chatMessages, setChatMessages] = useState([]);
 
   console.log({ chatMessages });
 
-  const handleUserJoined = () => {
-    const socket = io("http://localhost:5500");
-    socket.on("connect", () => {
-      setJoined(true);
-    });
-    socket.on("connect_error", () => {
-      setTimeout(() => socket.connect(), 5500);
-    });
+  const handleRoomCreated = (username: string, roomName: string) => {
+    axios
+      .post("http://localhost:5000/chatroom/create", {
+        username,
+        roomName,
+      })
+      .then((res) => {
+        setRoom({
+          roomName,
+          roomId: res.data.roomId,
+          userId: res.data.userId,
+        });
+        const socket = io("http://localhost:5500");
+        socket.on("connect", () => {
+          setJoined(true);
+        });
+        socket.on("connect_error", () => {
+          setTimeout(() => socket.connect(), 5500);
+        });
 
-    socketChannel = socket.on("chat-room", (message: string) => {
-      setChatMessages((prevState) => [...prevState, message]);
-      console.log(chatMessages);
-    });
-    socketChannel.connect();
-    socket.on("disconnect", () => {
-      console.log("disconnected");
-    });
+        socketChannel = socket.on("chat-room", (message: string) => {
+          setChatMessages((prevState) => [...prevState, message]);
+        });
+        socket.on("disconnect", () => {
+          console.log("disconnected");
+        });
+      });
+  };
+
+  const handleUserJoined = (username: string, roomId: string) => {
+    axios
+      .post("http://localhost:5000/chatroom/join", {
+        username,
+        roomId,
+      })
+      .then((res) => {
+        setRoom({
+          roomName: res.data.roomName,
+          roomId: res.data.roomId,
+          userId: res.data.userId,
+        });
+        const socket = io("http://localhost:5500");
+        socket.on("connect", () => {
+          setJoined(true);
+        });
+        socket.on("connect_error", () => {
+          setTimeout(() => socket.connect(), 5500);
+        });
+
+        socketChannel = socket.on("chat-room", (message: string) => {
+          setChatMessages((prevState) => [...prevState, message]);
+        });
+        socket.on("disconnect", () => {
+          console.log("disconnected");
+        });
+      });
   };
 
   const handleMessageSent = (message: string) => {
@@ -39,16 +84,41 @@ export const Chat = () => {
     }
   };
 
+  const handleJoinModeClicked = () => setGateMode(GateModes.JOIN);
+
+  const handleCreateModeClicked = () => setGateMode(GateModes.CREATE);
+
   return (
     <>
-      {!joined && (
+      {gateMode === undefined && (
+        <>
+          <button onClick={handleJoinModeClicked}>Join Room</button>
+
+          <button onClick={handleCreateModeClicked}>Create Room</button>
+        </>
+      )}
+
+      {!joined && gateMode !== undefined && (
         <section>
-          <Gate onJoin={handleUserJoined}></Gate>
+          <Gate
+            gateMode={gateMode}
+            onCreate={handleRoomCreated}
+            onJoin={handleUserJoined}
+          ></Gate>
+          <button
+            onClick={() => {
+              setGateMode(undefined);
+            }}
+          >
+            Back
+          </button>
         </section>
       )}
 
       {joined && (
         <section>
+          <h1>room name: {room.roomName}</h1>
+
           <UsersList></UsersList>
 
           <UserMessages messages={chatMessages}></UserMessages>
--- client/src/pages/Chat/components/Gate/Gate.tsx
+++ client/src/pages/Chat/components/Gate/Gate.tsx
@@ -2,77 +2,200 @@ import { FC } from "react";
 import { Formik, FormikHelpers } from "formik";
 import * as Yup from "yup";
 
-type FormValues = {
+type CreateFormValues = {
   username: string;
+  roomName: string;
 };
+
+type JoinFormValues = {
+  username: string;
+  roomId: string;
+};
+
+export enum GateModes {
+  CREATE = 0,
+  JOIN = 1,
+}
+
+const createSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(2, "short-input")
+    .max(50, "long-input")
+    .required("required-input"),
+  roomName: Yup.string()
+    .min(2, "short-input")
+    .max(50, "long-input")
+    .required("required-input"),
+});
 
 const joinSchema = Yup.object().shape({
   username: Yup.string()
     .min(2, "short-input")
     .max(50, "long-input")
     .required("required-input"),
+  roomId0: Yup.string()
+    .min(2, "short-input")
+    .max(50, "long-input")
+    .required("required-input"),
 });
 
 type Props = {
-  onJoin: (username: string) => void;
+  onCreate?: (username: string, roomName: string) => void;
+  onJoin?: (username: string, roomId: string) => void;
+  gateMode: GateModes;
 };
 
-export const Gate: FC<Props> = ({ onJoin }) => {
-  const handleJoinSubmitted = (
-    values: FormValues,
-    { setSubmitting }: FormikHelpers<FormValues>
+export const Gate: FC<Props> = ({
+  onCreate,
+  gateMode = GateModes.CREATE,
+  onJoin,
+}) => {
+  const handleCreatSubmitted = (
+    values: CreateFormValues,
+    { setSubmitting }: FormikHelpers<CreateFormValues>
   ) => {
-    onJoin(values.username);
+    if (onCreate) onCreate(values.username, values.roomName);
     setSubmitting(false);
+  };
+
+  const handleJoinSubmitted = (values: JoinFormValues) => {
+    console.log("awdawd");
+    if (onJoin) onJoin(values.username, values.roomId);
   };
 
   return (
     <>
       <div className='gate'>
-        <Formik
-          initialValues={{ username: "" }}
-          validationSchema={joinSchema}
-          onSubmit={handleJoinSubmitted}
-        >
-          {({
-            errors,
-            touched,
-            values,
-            handleChange,
-            handleSubmit,
-            isSubmitting,
-          }) => (
-            <form onSubmit={handleSubmit}>
-              <div className='username-form'>
-                <header>
-                  <h1>Join</h1>
-                </header>
+        {gateMode === GateModes.CREATE && (
+          <Formik
+            initialValues={{ username: "", roomName: "" }}
+            validationSchema={createSchema}
+            onSubmit={handleCreatSubmitted}
+          >
+            {({
+              errors,
+              touched,
+              values,
+              handleChange,
+              handleSubmit,
+              isSubmitting,
+              handleBlur,
+            }) => (
+              <form onSubmit={handleSubmit}>
+                <div className='username-form'>
+                  <header>
+                    <h1>Create</h1>
+                  </header>
 
-                <main>
-                  <label htmlFor='username'>Username</label>
-                  <input
-                    id='username'
-                    type='text'
-                    name='username'
-                    value={values.username}
-                    onChange={handleChange}
-                  />
-                  <span className='input-error'>
-                    {touched && errors && errors.username ? (
-                      <div className='error'>{errors.username}</div>
-                    ) : null}
-                  </span>
-                </main>
+                  <main>
+                    <div className='input-group'>
+                      <label htmlFor='username'>Username</label>
+                      <input
+                        id='username'
+                        type='text'
+                        name='username'
+                        value={values.username}
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                      />
+                      <span className='input-error'>
+                        {touched.username && errors.username && errors.username}
+                      </span>
+                    </div>
+                    <div className='input-group'>
+                      <label htmlFor='roomName'>Room Name</label>
+                      <input
+                        id='roomName'
+                        type='text'
+                        name='roomName'
+                        value={values.roomName}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                      />
+                      <span className='input-error'>
+                        {touched.roomName && errors.roomName && errors.roomName}
+                      </span>
+                    </div>
+                  </main>
 
-                <footer>
-                  <button type='submit' disabled={isSubmitting}>
-                    Join
-                  </button>
-                </footer>
-              </div>
-            </form>
-          )}
-        </Formik>
+                  <footer>
+                    <button type='submit' disabled={isSubmitting}>
+                      Create
+                    </button>
+                  </footer>
+                </div>
+              </form>
+            )}
+          </Formik>
+        )}
+        {gateMode === GateModes.JOIN && (
+          <Formik
+            initialValues={{ username: "", roomId: "" }}
+            validationSchema={joinSchema}
+            onSubmit={handleJoinSubmitted}
+          >
+            {({
+              errors,
+              touched,
+              values,
+              handleChange,
+              handleSubmit,
+              isSubmitting,
+              handleBlur,
+            }) => (
+              <form onSubmit={handleSubmit}>
+                <div className='username-form'>
+                  <header>
+                    <h1>Join</h1>
+                  </header>
+
+                  <main>
+                    <div className='input-group'>
+                      <label htmlFor='username'>Username</label>
+                      <input
+                        id='username'
+                        type='text'
+                        name='username'
+                        value={values.username}
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                      />
+                      <span className='input-error'>
+                        {touched.username && errors.username && errors.username}
+                      </span>
+                    </div>
+                    <div className='input-group'>
+                      <label htmlFor='roomId'>Room Name</label>
+                      <input
+                        id='roomId'
+                        type='text'
+                        name='roomId'
+                        value={values.roomId}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                      />
+                      <span className='input-error'>
+                        {touched.roomId && errors.roomId && errors.roomId}
+                      </span>
+                    </div>
+                  </main>
+
+                  <footer>
+                    <button
+                      onClick={() => {
+                        handleJoinSubmitted(values);
+                      }}
+                      type='submit'
+                      disabled={isSubmitting}
+                    >
+                      Join
+                    </button>
+                  </footer>
+                </div>
+              </form>
+            )}
+          </Formik>
+        )}
       </div>
     </>
   );
